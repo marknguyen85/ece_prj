d76d43d103a7d0f5970a9370a492c2d204cdc2e4
huyvan123 <vannguyen231195@gmail.com>
2018-09-22 16:58:49 +0700

    change link

--- web/src/js/charts.js
+++ web/src/js/charts.js
@@ -19,42 +19,42 @@ var random = function random() {
 var lineChart = new Chart($('#monthChartExam'), {
   type: 'line',
   data: {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    labels: ['January', 'February', 'March', 'April', 'May', 'June'],
     datasets: [{
-      label: 'My First dataset',
+      label: 'Codding',
       backgroundColor: 'rgba(220, 220, 220, 0.2)',
-      borderColor: 'rgba(220, 220, 220, 1)',
-      pointBackgroundColor: 'rgba(220, 220, 220, 1)',
-      pointBorderColor: '#fff'
-      // data: [random(), random(), random(), random(), random(), random(), random()]
-    }, {
-      label: 'My Second dataset',
-      backgroundColor: 'rgba(151, 187, 205, 0.2)',
-      borderColor: 'rgba(151, 187, 205, 1)',
-      pointBackgroundColor: 'rgba(151, 187, 205, 1)',
+      borderColor: 'green',
+      pointBackgroundColor: 'green',
       pointBorderColor: '#fff',
-      // data: [random(), random(), random(), random(), random(), random(), random()]
+      data: [random(), random(), random(), random(), random(), random(), random()]
+    }, {
+      label: 'IQ',
+      backgroundColor: 'rgba(151, 187, 205, 0.2)',
+      borderColor: 'black',
+      pointBackgroundColor: 'black',
+      pointBorderColor: '#fff',
+      data: [random(), random(), random(), random(), random(), random(), random()]
     }, {
       label: 'Database',
       backgroundColor: 'rgba(151, 187, 205, 0.2)',
       borderColor: 'red',
       pointBackgroundColor: 'red',
       pointBorderColor: '#fff',
-      // data: [random(), random(), random(), random(), random(), random(), random()]
+      data: [random(), random(), random(), random(), random(), random(), random()]
     }, {
       label: 'Attitude',
       backgroundColor: 'rgba(151, 187, 205, 0.2)',
       borderColor: 'blue',
       pointBackgroundColor: 'blue',
       pointBorderColor: '#fff',
-      // data: [random(), random(), random(), random(), random(), random(), random()]
+      data: [random(), random(), random(), random(), random(), random(), random()]
     }, {
       label: 'Manager',
       backgroundColor: 'rgba(151, 187, 205, 0.2)',
       borderColor: 'yellow',
       pointBackgroundColor: 'yellow',
       pointBorderColor: '#fff',
-      // data: [random(), random(), random(), random(), random(), random(), random()]
+      data: [random(), random(), random(), random(), random(), random(), random()]
     }]
   },
   options: {
@@ -107,13 +107,14 @@ var radarChart = new Chart($('#canvasCapaci'), {
   data: {
     labels: ['Chuyên môn', 'IQ', 'Chuyên cần', 'Thái độ', 'Ngoại ngữ'],
     datasets: [{
-      label: 'Vi Văn Thức',
-      backgroundColor: 'rgba(220, 220, 220, 0.2)',
+      label: 'Nguyễn Văn A',
+      backgroundColor: 'rgba(151, 187, 205, 0.2)',
       borderColor: 'black',
-      pointBackgroundColor: 'rgba(220, 220, 220, 1)',
-      pointBorderColor: 'black',
-      pointHighlightFill: 'black',
-      pointHighlightStroke: 'rgba(151, 187, 205, 1)'
+      pointBackgroundColor: 'black',
+      pointBorderColor: '#fff',
+      pointHighlightFill: '#fff',
+      pointHighlightStroke: 'rgba(220, 220, 220, 1)',
+      data: [50, 60, 70, 60, 50]
     }]
   },
   options: {
